56c3cd99017f6679cce17ddee0f2e2850d8fec80
dpak-maurya <dpak.m932@gmail.com>
2026-04-22 14:35:19 +0530

    fix: stabilize payment automation and sync selectors with new IRCTC UI

--- chrome-extension/webpack.content.js
+++ chrome-extension/webpack.content.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'content.js',
+    filename: '[name].js',
     clean: true
   },
   mode: 'production',
